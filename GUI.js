d04746b3e9ef6ff0095524038bdbe2d5a0e063ee
Bnn16 <Bogdan.Nikolov4@outlook.com>
2024-05-10 16:03:47 +0200

    found duration of a video + playing it, TODO: work on pausing and playing

--- GUI.js
+++ GUI.js
@@ -3,8 +3,10 @@ import contrib from "blessed-contrib";
 import { authorize } from "./index.js";
 import { searchVideos, getPlaylists, getPlaylistItems } from "./requests.js";
 
-const screen = blessed.screen();
+import { spawn } from "child_process";
+import { writeFile } from "fs";
 
+const screen = blessed.screen();
 screen.title = "Youtubeyy";
 
 //grid.set(row, col, rowSpan, colSpan, obj, opts)
@@ -125,6 +127,7 @@ playLists.on("select", async (data) => {
         channel: item.snippet.channelTitle,
         vidId: item.snippet.resourceId.videoId,
         string: `${item.snippet.title} - ${item.snippet.channelTitle}`,
+        length: item.contentDetails.duration,
       });
     });
     songsBox.clearItems();
@@ -133,10 +136,46 @@ playLists.on("select", async (data) => {
   });
 });
 
+let ffplayProcess;
+let durValue;
 songsBox.on("select", (data) => {
   const textTitle = data.getText();
   const index = mySongs.findIndex((song) => song.string === textTitle);
   //textBox.setValue(mySongs[index].vidId);
+  // ffplay $(youtube-dl --format 249 --get-url https://www.youtube.com/watch\?\v=p0u8ESAThSs) -nodisp
+
+  const videoURL = `https://www.youtube.com/watch?v=${mySongs[index].vidId}`;
+
+  const youtubeDlProcess = spawn("youtube-dl", [
+    "--format",
+    "249",
+    "--get-url",
+    videoURL,
+  ]);
+
+  let videoUrl = "";
+  const regex = /dur=([^&]+)/;
+
+  youtubeDlProcess.stdout.on("data", (data) => {
+    videoUrl += data.toString();
+  });
+
+  youtubeDlProcess.on("close", (code) => {
+    if (code === 0) {
+      const durMatch = videoUrl.match(regex);
+
+      if (durMatch) {
+        durValue = durMatch[1] / 60;
+      }
+      ffplayProcess = spawn("ffplay", [videoUrl.trim(), "-nodisp"]);
+
+      ffplayProcess.on("close", (code) => {
+        console.log(`ffplay process exited with code ${code}`);
+      });
+    } else {
+      console.error(`youtube-dl process exited with code ${code}`);
+    }
+  });
   // this is where we will play the song with a sub process
   screen.render();
 });
@@ -167,4 +206,15 @@ if (textBox.focused) {
     screen.render();
   });
 }
+
+screen.key(["p"], function (ch, key) {
+  writeFile("temp.txt", JSON.stringify(durValue), (err) => {
+    if (err) throw err;
+  });
+  if (ffplayProcess) {
+    ffplayProcess.kill();
+    ffplayProcess = spawn("ffplay", ["-nodisp"]);
+  }
+});
+
 screen.render();
